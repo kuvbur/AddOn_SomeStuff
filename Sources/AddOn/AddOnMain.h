b93573e8348eb4b21528a36ef0f36f16cddf9e64
ufoHub <ufo735@gmail.com>
2021-09-28 10:52:58 +0500

    AC_25
    Собрал но забыл слить на гитхаб как вернусь с работы выложу

--- Sources/AddOn/AddOnMain.h
+++
@@ -1,4 +0,0 @@
-void	Do_ElementMonitor();
-GSErrCode __ACENV_CALL	ElementEventHandlerProc(const API_NotifyElementType* elemType);
-static GSErrCode __ACENV_CALL    ProjectEventHandlerProc(API_NotifyEventID notifID, Int32 param);
-static GSErrCode MenuCommandHandler(const API_MenuParams* menuParams);
