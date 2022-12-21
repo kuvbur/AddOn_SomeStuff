4077febbdf443ae093595bbc02a592478a19214f
Дмитрий <kuvbur@gmail.com>
2022-12-21 22:32:49 +0300

    Update APICommon25.h

--- Sources/AddOn/APICommon25.h
+++ Sources/AddOn/APICommon25.h
@@ -24,12 +24,21 @@
 /* -- Constants ------------------------------- */
 
 #if !defined (_Geometry_Test_TRANSL_)
-#define		PI			3.141592653589793		/* The Ludolph's constant	 */
-#define		RADDEG		57.29577951308232		/* Radian->degree conversion */
-#define		DEGRAD		0.017453292519943295	/* Degree->radian conversion */
-
-#define		EPS			1e-5
-#define 	MiddleEps6	1e-6
+#ifndef PI
+	#define		PI			3.141592653589793		/* The Ludolph's constant	 */
+#endif // !PI
+#ifndef RADDEG
+	#define		RADDEG		57.29577951308232		/* Radian->degree conversion */
+#endif // !RADDEG
+#ifndef DEGRAD
+	#define		DEGRAD		0.017453292519943295	/* Degree->radian conversion */
+#endif // !DEGRAD
+#ifndef EPS
+	#define		EPS			1e-5
+#endif // !EPS
+#ifndef MiddleEps6
+	#define 	MiddleEps6	1e-6
+#endif // !MiddleEps6
 #endif
 
 
