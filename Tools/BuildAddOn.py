0446eb7581caef276ae769cc596d95571f526b59
Дмитрий <kuvbur@gmail.com>
2023-12-03 23:48:08 +0300

    Переход на новый метод сборки

---
+++ Tools/BuildAddOn.py
@@ -0,0 +1,378 @@
+import argparse
+import json
+import os
+import pathlib
+import platform
+import shutil
+import subprocess
+import sys
+import urllib.parse
+import urllib.request
+import zipfile
+import requests
+API_ENDPOINT = 'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={}'
+
+
+def _extract_filename_yadisk_link(direct_link):
+    for chunk in direct_link.strip().split('&'):
+        if chunk.startswith('filename='):
+            return chunk.split('=')[1]
+    return None
+
+
+def DownloadFromYadisk(url, dest):
+    pk_request = requests.get(API_ENDPOINT.format(url))
+    direct_link = pk_request.json().get('href')
+    if direct_link:
+        filename = _extract_filename_yadisk_link(direct_link)
+        filePath = os.path.join(dest, filename)
+        download = requests.get(direct_link)
+        with open(filePath, 'wb') as out_file:
+            out_file.write(download.content)
+        print('Downloaded "{}" to "{}"'.format(url, filePath))
+        return filePath
+    else:
+        print('Failed to download "{}"'.format(url))
+        return None
+
+
+def ParseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--configFile', dest='configFile',
+                        required=True, help='JSON Configuration file')
+    parser.add_argument('-v', '--acVersion', dest='acVersion', nargs='+',
+                        type=str, required=False, help='Archicad version number list. Ex: 26 27')
+    parser.add_argument('-l', '--language', dest='language', nargs='+', type=str, required=False,
+                        help='Add-On language code list. Ex: INT GER. Specify ALL for all languages in the configfile.')
+    parser.add_argument('-d', '--devKitPath', dest='devKitPath',
+                        type=str, required=False, help='Path to local APIDevKit')
+    parser.add_argument('-r', '--release', dest='release', required=False,
+                        action='store_true', help='Build in localized Release mode.')
+    parser.add_argument('-p', '--package', dest='package', required=False,
+                        action='store_true', help='Create zip archive.')
+    args = parser.parse_args()
+
+    if args.devKitPath is not None:
+        if args.acVersion is None:
+            raise Exception(
+                'Must provide one Archicad version with local APIDevKit option!')
+        if len(args.acVersion) != 1:
+            raise Exception(
+                'Only one Archicad version supported with local APIDevKit option!')
+
+    return args
+
+
+def PrepareParameters(args):
+    # Check platform operating system
+    platformName = None
+    if platform.system() == 'Windows':
+        platformName = 'WIN'
+    elif platform.system() == 'Darwin':
+        platformName = 'MAC'
+    # Load config data
+    configPath = pathlib.Path(args.configFile)
+    if configPath.is_dir():
+        raise Exception(f'{configPath} is a directory!')
+    configFile = open(configPath)
+    configData = json.load(configFile)
+    addOnName = configData['addOnName']
+    acVersionList = None
+    languageList = None
+
+    if args.acVersion:
+        acVersionList = args.acVersion
+    else:
+        acVersionList = []
+        for version in configData['devKitLinks'][platformName]:
+            acVersionList.append(version)
+
+    # Get needed language codes
+    if args.release:
+        configLangUpper = [lang.upper() for lang in configData['languages']]
+        languageList = ['ALL']
+        if args.language is not None:
+            languageList = [lang.upper() for lang in args.language]
+
+        if 'ALL' in languageList:
+            languageList = configLangUpper
+        else:
+            for lang in languageList:
+                if lang not in configLangUpper:
+                    raise Exception('Language not supported!')
+
+    return [configData, platformName, addOnName, acVersionList, languageList]
+
+
+def PrepareDirectories(args, configData, platformName, addOnName, acVersionList):
+    # Create directory for Build and Package
+    workspaceRootFolder = pathlib.Path(
+        __file__).parent.absolute().parent.absolute()
+    buildFolder = workspaceRootFolder / 'Build'
+    packageRootFolder = buildFolder / 'Package' / addOnName
+    devKitFolderList = {}
+
+    if not buildFolder.exists():
+        buildFolder.mkdir(parents=True)
+
+    if args.package:
+        if (packageRootFolder).exists():
+            shutil.rmtree(packageRootFolder)
+
+    # Set APIDevKit directory if local is used, else create new directories
+    if args.devKitPath is not None:
+        devKitPath = pathlib.Path(args.devKitPath)
+        if not devKitPath.is_dir():
+            raise Exception(f'{devKitPath} is not a directory!')
+        devKitFolderList[acVersionList[0]] = devKitPath
+    else:
+        # For every ACVersion
+        # Check if APIDevKitLink is provided
+        # Create directory for APIDevKit
+        # Download APIDevKit
+        for version in acVersionList:
+            if version in configData['devKitLinks'][platformName]:
+
+                devKitFolder = buildFolder / 'DevKit' / f'APIDevKit-{version}'
+                if not devKitFolder.exists():
+                    devKitFolder.mkdir(parents=True)
+
+                devKitFolderList[version] = devKitFolder
+                DownloadAndUnzip(
+                    configData['devKitLinks'][platformName][version], devKitFolder)
+
+            else:
+                raise Exception('APIDevKit download link not provided!')
+
+    return [workspaceRootFolder, buildFolder, packageRootFolder, devKitFolderList]
+
+
+def DownloadAndUnzip(url, dest):
+    if 'disk.yandex' in url:
+        filePath = DownloadFromYadisk(url, dest)
+        fileName = filePath.split('/')[-1]
+    else:
+        fileName = url.split('/')[-1]
+        filePath = pathlib.Path(dest, fileName)
+        if filePath.exists():
+            return
+        print(f'Downloading {fileName}')
+        urllib.request.urlretrieve(url, filePath)
+
+    print(f'Unzipping {fileName}')
+    if platform.system() == 'Windows':
+        with zipfile.ZipFile(filePath, 'r') as zip:
+            zip.extractall(dest)
+    elif platform.system() == 'Darwin':
+        subprocess.call([
+            'unzip', '-qq', filePath,
+            '-d', dest
+        ])
+
+
+def GetInstalledVisualStudioGenerator():
+    vsWherePath = pathlib.Path(
+        os.environ["ProgramFiles(x86)"]) / 'Microsoft Visual Studio' / 'Installer' / 'vswhere.exe'
+    if not vsWherePath.exists():
+        raise Exception('Microsoft Visual Studio Installer not found!')
+    vsWhereOutputStr = subprocess.check_output(
+        [vsWherePath, '-sort', '-format', 'json']).decode('utf-8', 'ignore')
+    vsWhereOutput = json.loads(vsWhereOutputStr)
+    if len(vsWhereOutput) == 0:
+        raise Exception('No installed Visual Studio detected!')
+    vsVersion = vsWhereOutput[0]['installationVersion'].split('.')[0]
+    if vsVersion == '17':
+        return 'Visual Studio 17 2022'
+    elif vsVersion == '16':
+        return 'Visual Studio 16 2019'
+    else:
+        raise Exception('Installed Visual Studio version not supported!')
+
+
+def GetProjectGenerationParams(workspaceRootFolder, buildPath, platformName, devKitFolder, version, languageCode, optionalParams):
+    # Add params to configure cmake
+    projGenParams = [
+        'cmake',
+        '-B', str(buildPath)
+    ]
+
+    if platformName == 'WIN':
+        vsGenerator = GetInstalledVisualStudioGenerator()
+        projGenParams.append(f'-G {vsGenerator}')
+        toolset = 'v142'
+        if int(version) < 25:
+            toolset = 'v141'
+        if int(version) < 23:
+            toolset = 'v140'
+        projGenParams.append(f'-T {toolset}')
+    elif platformName == 'MAC':
+        projGenParams.extend(['-G', 'Xcode'])
+
+    projGenParams.append(
+        f'-DAC_API_DEVKIT_DIR={str (devKitFolder / "Support")}')
+
+    if languageCode is not None:
+        projGenParams.append(f'-DAC_ADDON_LANGUAGE={languageCode}')
+
+    if optionalParams is not None:
+        for key in optionalParams:
+            projGenParams.append(f'-D{key}={optionalParams[key]}')
+
+    projGenParams.append(str(workspaceRootFolder))
+
+    return projGenParams
+
+
+def BuildAddOn(configData, platformName, workspaceRootFolder, buildFolder, devKitFolder, version, configuration, languageCode=None):
+    addOnName = configData['addOnName']
+    optionalParams = None
+    if 'addOnSpecificCMakeParameterEnvVars' in configData:
+        optionalParams = {}
+        for key in configData['addOnSpecificCMakeParameterEnvVars']:
+            value = os.environ.get(key)
+            if value is None:
+                raise Exception(f'{key} - Environment variable not found!')
+            optionalParams[key] = value
+
+    buildPath = buildFolder / addOnName / version
+    if languageCode is not None:
+        buildPath = buildPath / languageCode
+
+    # Add params to configure cmake
+    projGenParams = GetProjectGenerationParams(
+        workspaceRootFolder, buildPath, platformName, devKitFolder, version, languageCode, optionalParams)
+    projGenResult = subprocess.call(projGenParams)
+    if projGenResult != 0:
+        raise Exception('Failed to generate project!')
+
+    # Add params to build AddOn
+    buildParams = [
+        'cmake',
+        '--build', str(buildPath),
+        '--config', configuration
+    ]
+
+    buildResult = subprocess.call(buildParams)
+    if buildResult != 0:
+        raise Exception('Failed to build project!')
+
+
+def BuildAddOns(args, configData, platformName, languageList, workspaceRootFolder, buildFolder, devKitFolderList):
+    # At this point, devKitFolderList dictionary has all provided ACVersions as keys
+    # For every ACVersion
+    # If release, build Add-On for all languages with RelWithDebInfo configuration
+    # Else build Add-On with Debug and RelWithDebInfo configurations, without language specified
+    # In each case, if package creation is enabled, copy the .apx/.bundle files to the Package directory
+    try:
+        for version in devKitFolderList:
+            devKitFolder = devKitFolderList[version]
+
+            if args.release is True:
+                for languageCode in languageList:
+                    BuildAddOn(configData, platformName, workspaceRootFolder, buildFolder,
+                               devKitFolder, version, 'RelWithDebInfo', languageCode)
+
+            else:
+                BuildAddOn(configData, platformName, workspaceRootFolder,
+                           buildFolder, devKitFolder, version, 'Debug')
+                BuildAddOn(configData, platformName, workspaceRootFolder,
+                           buildFolder, devKitFolder, version, 'RelWithDebInfo')
+
+    except Exception as e:
+        raise e
+
+
+def Check7ZInstallation():
+    try:
+        subprocess.call('7z', stdout=subprocess.DEVNULL)
+    except:
+        raise Exception('7Zip not installed!')
+
+
+def CopyResultToPackage(packageRootFolder, buildFolder, version, addOnName, platformName, configuration, languageCode=None, isRelease=False):
+    packageFolder = packageRootFolder / version
+    sourceFolder = buildFolder / addOnName / version
+
+    if languageCode is not None:
+        packageFolder = packageFolder / languageCode
+        sourceFolder = sourceFolder / languageCode
+    sourceFolder = sourceFolder / configuration
+
+    if not packageFolder.exists():
+        packageFolder.mkdir(parents=True)
+
+    fileName = addOnName
+    if not isRelease:
+        fileName = f'{fileName}_{configuration}'
+
+    if platformName == 'WIN':
+        shutil.copy(
+            sourceFolder / f'{addOnName}.apx',
+            packageFolder / f'{fileName}.apx',
+        )
+        if configuration == 'Debug':
+            shutil.copy(
+                sourceFolder / f'{addOnName}.pdb',
+                packageFolder / f'{fileName}.pdb',
+            )
+
+    elif platformName == 'MAC':
+        subprocess.call([
+            'cp', '-r',
+            sourceFolder / f'{addOnName}.bundle',
+            packageFolder / f'{fileName}.bundle'
+        ])
+
+
+# Zip packages
+def PackageAddOns(args, addOnName, platformName, acVersionList, languageList, buildFolder, packageRootFolder):
+    Check7ZInstallation()
+
+    for version in acVersionList:
+        if args.release:
+            for languageCode in languageList:
+                CopyResultToPackage(packageRootFolder, buildFolder, version,
+                                    addOnName, platformName, 'RelWithDebInfo', languageCode, True)
+        else:
+            CopyResultToPackage(packageRootFolder, buildFolder,
+                                version, addOnName, platformName, 'Debug')
+            CopyResultToPackage(packageRootFolder, buildFolder,
+                                version, addOnName, platformName, 'RelWithDebInfo')
+
+        buildType = 'Release' if args.release else 'Daily'
+        subprocess.call([
+            '7z', 'a',
+            str(packageRootFolder.parent /
+                f'{addOnName}-{version}_{buildType}_{platformName}.zip'),
+            str(packageRootFolder / version / '*')
+        ])
+
+
+def Main():
+    try:
+        args = ParseArguments()
+
+        [configData, platformName, addOnName, acVersionList,
+            languageList] = PrepareParameters(args)
+
+        [workspaceRootFolder, buildFolder, packageRootFolder, devKitFolderList] = PrepareDirectories(
+            args, configData, platformName, addOnName, acVersionList)
+        os.chdir(workspaceRootFolder)
+
+        BuildAddOns(args, configData, platformName, languageList,
+                    workspaceRootFolder, buildFolder, devKitFolderList)
+
+        if args.package:
+            PackageAddOns(args, addOnName, platformName, acVersionList,
+                          languageList, buildFolder, packageRootFolder)
+
+        print('Build succeeded!')
+        sys.exit(0)
+
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    Main()
---
+++ Tools/CMakeCommon.cmake
@@ -0,0 +1,220 @@
+function (SetGlobalCompilerDefinitions)
+
+    if (WIN32)
+        add_definitions (-DUNICODE -D_UNICODE)
+    else ()
+        add_definitions (-Dmacintosh=1)
+    endif ()
+    add_definitions (-DACExtension)
+
+endfunction ()
+
+function (SetCompilerOptions target acVersion)
+
+    if (${acVersion} LESS 27)
+        target_compile_features (${target} PUBLIC cxx_std_14)
+    else ()
+        target_compile_features (${target} PUBLIC cxx_std_17)
+    endif ()
+    target_compile_options (${target} PUBLIC "$<$<CONFIG:Debug>:-DDEBUG>")
+    if (WIN32)
+        target_compile_options (${target} PUBLIC /W3 /WX
+            /Zc:wchar_t-
+            /wd4499
+            /EHsc
+			/wd4003
+			/wd5208
+			/wd4996
+			/bigobj
+            -D_CRT_SECURE_NO_WARNINGS
+        )
+    else ()
+        target_compile_options (${target} PUBLIC -Wall -Wextra -Werror
+            -fvisibility=hidden
+            -Wno-multichar
+            -Wno-ctor-dtor-privacy
+            -Wno-invalid-offsetof
+            -Wno-ignored-qualifiers
+            -Wno-reorder
+            -Wno-overloaded-virtual
+            -Wno-unused-parameter
+            -Wno-unused-value
+            -Wno-unused-private-field
+            -Wno-unused-but-set-variable
+            -Wno-deprecated
+            -Wno-unknown-pragmas
+            -Wno-missing-braces
+            -Wno-missing-field-initializers
+            -Wno-non-c-typedef-for-linkage
+            -Wno-uninitialized-const-reference
+            -Wno-shorten-64-to-32
+            -Wno-sign-compare
+            -Wno-switch
+        )
+        if (${acVersion} LESS_EQUAL "24")
+            target_compile_options (${target} PUBLIC -Wno-non-c-typedef-for-linkage)
+        endif ()
+    endif ()
+    add_definitions (-DAC_${acVersion})
+endfunction ()
+
+function (DetectACVersion devKitDir acVersion)
+
+    set (ACAPIncFileLocation ${devKitDir}/Inc/ACAPinc.h)
+    if (EXISTS ${ACAPIncFileLocation})
+        file (READ ${ACAPIncFileLocation} ACAPIncContent)
+        string (REGEX MATCHALL "#define[ \t]+ServerMainVers_([0-9][0-9])" VersionList ${ACAPIncContent})
+        set (${acVersion} ${CMAKE_MATCH_1} PARENT_SCOPE)
+    else ()
+        message (FATAL_ERROR "Failed to detect Archicad version, please check the value of the AC_API_DEVKIT_DIR variable.")
+    endif ()
+
+endfunction ()
+
+function (LinkGSLibrariesToProject acVersion devKitDir addOnName)
+
+    if (WIN32)
+        set_target_properties(${addOnName} PROPERTIES
+        VS_DEBUGGER_WORKING_DIRECTORY "$(ProjectDir)"
+        VS_DEBUGGER_COMMAND "$ENV{ProgramFiles}/GRAPHISOFT/ARCHICAD ${ARCHICAD_VERSION}/ARCHICAD.exe"
+        VS_DEBUGGER_COMMAND_ARGUMENTS "$(ProjectDir)test.pln"
+        )
+        if (${acVersion} LESS 27)
+            target_link_libraries (${addOnName}
+                "$<$<CONFIG:Debug>:${devKitDir}/Lib/Win/ACAP_STATD.lib>"
+                "$<$<CONFIG:Release>:${devKitDir}/Lib/Win/ACAP_STAT.lib>"
+                "$<$<CONFIG:RelWithDebInfo>:${devKitDir}/Lib/Win/ACAP_STAT.lib>"
+            )
+        else ()
+            target_link_libraries (${addOnName}
+                "$<$<CONFIG:Debug>:${devKitDir}/Lib/ACAP_STATD.lib>"
+                "$<$<CONFIG:Release>:${devKitDir}/Lib/ACAP_STAT.lib>"
+                "$<$<CONFIG:RelWithDebInfo>:${devKitDir}/Lib/ACAP_STAT.lib>"
+            )
+        endif ()
+    else ()
+        find_library (CocoaFramework Cocoa)
+        if (${acVersion} LESS 27)
+            target_link_libraries (${addOnName}
+                "${devKitDir}/Lib/Mactel/libACAP_STAT.a"
+                ${CocoaFramework}
+            )
+        else ()
+            target_link_libraries (${addOnName}
+                "${devKitDir}/Lib/libACAP_STAT.a"
+                ${CocoaFramework}
+            )
+        endif ()
+    endif ()
+
+    file (GLOB ModuleFolders ${devKitDir}/Modules/*)
+    target_include_directories (${addOnName} PUBLIC ${ModuleFolders})
+    if (WIN32)
+        file (GLOB LibFilesInFolder ${devKitDir}/Modules/*/*/*.lib)
+        target_link_libraries (${addOnName} ${LibFilesInFolder})
+    else ()
+        file (GLOB LibFilesInFolder
+            ${devKitDir}/Frameworks/*.framework
+            ${devKitDir}/Frameworks/*.dylib
+        )
+        target_link_libraries (${addOnName} ${LibFilesInFolder})
+    endif ()
+
+endfunction ()
+
+function (GenerateAddOnProject acVersion devKitDir addOnName addOnSourcesFolder addOnResourcesFolder addOnLanguage)
+
+    find_package (Python COMPONENTS Interpreter)
+
+    set (ResourceObjectsDir ${CMAKE_BINARY_DIR}/ResourceObjects)
+    set (ResourceStampFile "${ResourceObjectsDir}/AddOnResources.stamp")
+
+    file (GLOB AddOnImageFiles CONFIGURE_DEPENDS
+        ${addOnResourcesFolder}/RFIX/Images/*.svg
+    )
+    if (WIN32)
+        file (GLOB AddOnResourceFiles CONFIGURE_DEPENDS
+            ${addOnResourcesFolder}/R${addOnLanguage}/*.grc
+            ${addOnResourcesFolder}/RFIX/*.grc
+            ${addOnResourcesFolder}/RFIX.win/*.rc2
+            ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/*.py
+        )
+    else ()
+        file (GLOB AddOnResourceFiles CONFIGURE_DEPENDS
+            ${addOnResourcesFolder}/R${addOnLanguage}/*.grc
+            ${addOnResourcesFolder}/RFIX/*.grc
+            ${addOnResourcesFolder}/RFIX.mac/*.plist
+            ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/*.py
+        )
+    endif ()
+
+    get_filename_component (AddOnSourcesFolderAbsolute "${CMAKE_CURRENT_LIST_DIR}/${addOnSourcesFolder}" ABSOLUTE)
+    get_filename_component (AddOnResourcesFolderAbsolute "${CMAKE_CURRENT_LIST_DIR}/${addOnResourcesFolder}" ABSOLUTE)
+    if (WIN32)
+        add_custom_command (
+            OUTPUT ${ResourceStampFile}
+            DEPENDS ${AddOnResourceFiles} ${AddOnImageFiles}
+            COMMENT "Compiling resources..."
+            COMMAND ${CMAKE_COMMAND} -E make_directory "${ResourceObjectsDir}"
+            COMMAND ${Python_EXECUTABLE} "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/CompileResources.py" "${addOnLanguage}" "${devKitDir}" "${AddOnSourcesFolderAbsolute}" "${AddOnResourcesFolderAbsolute}" "${ResourceObjectsDir}" "${ResourceObjectsDir}/${addOnName}.res"
+            COMMAND ${CMAKE_COMMAND} -E touch ${ResourceStampFile}
+        )
+    else ()
+        add_custom_command (
+            OUTPUT ${ResourceStampFile}
+            DEPENDS ${AddOnResourceFiles} ${AddOnImageFiles}
+            COMMENT "Compiling resources..."
+            COMMAND ${CMAKE_COMMAND} -E make_directory "${ResourceObjectsDir}"
+            COMMAND ${Python_EXECUTABLE} "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/CompileResources.py" "${addOnLanguage}" "${devKitDir}" "${AddOnSourcesFolderAbsolute}" "${AddOnResourcesFolderAbsolute}" "${ResourceObjectsDir}" "${CMAKE_BINARY_DIR}/$<CONFIG>/${addOnName}.bundle/Contents/Resources"
+            COMMAND ${CMAKE_COMMAND} -E copy "${devKitDir}/Inc/PkgInfo" "${CMAKE_BINARY_DIR}/$<CONFIG>/${addOnName}.bundle/Contents/PkgInfo"
+            COMMAND ${CMAKE_COMMAND} -E touch ${ResourceStampFile}
+        )
+    endif ()
+
+    file (GLOB AddOnHeaderFiles CONFIGURE_DEPENDS
+        ${addOnSourcesFolder}/*.h
+    )
+    file (GLOB AddOnSourceFiles CONFIGURE_DEPENDS
+        ${addOnSourcesFolder}/*.cpp
+    )
+    set (
+        AddOnFiles
+        ${AddOnHeaderFiles}
+        ${AddOnSourceFiles}
+        ${AddOnImageFiles}
+        ${AddOnResourceFiles}
+        ${ResourceStampFile}
+    )
+    
+    source_group ("Sources" FILES ${AddOnHeaderFiles} ${AddOnSourceFiles})
+    source_group ("Images" FILES ${AddOnImageFiles})
+    source_group ("Resources" FILES ${AddOnResourceFiles})
+    if (WIN32)
+        add_library (${addOnName} SHARED ${AddOnFiles})
+    else ()
+        add_library (${addOnName} MODULE ${AddOnFiles})
+    endif ()
+
+    set_target_properties (${addOnName} PROPERTIES OUTPUT_NAME ${addOnName})
+    if (WIN32)
+        set_target_properties (${addOnName} PROPERTIES SUFFIX ".apx")
+        set_target_properties (${addOnName} PROPERTIES RUNTIME_OUTPUT_DIRECTORY_$<CONFIG> "${CMAKE_BINARY_DIR}/$<CONFIG>")
+        target_link_options (${addOnName} PUBLIC "${ResourceObjectsDir}/${addOnName}.res")
+        target_link_options (${addOnName} PUBLIC /export:GetExportedFuncAddrs,@1 /export:SetImportedFuncAddrs,@2)
+    else ()
+        set_target_properties (${addOnName} PROPERTIES BUNDLE TRUE)
+        set_target_properties (${addOnName} PROPERTIES MACOSX_BUNDLE_INFO_PLIST "${CMAKE_CURRENT_LIST_DIR}/${addOnResourcesFolder}/RFIX.mac/Info.plist")
+        set_target_properties (${addOnName} PROPERTIES LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/$<CONFIG>")
+    endif ()
+
+    target_include_directories (${addOnName} PUBLIC
+        ${addOnSourcesFolder}
+        ${devKitDir}/Inc
+    )
+
+    LinkGSLibrariesToProject (${acVersion} ${devKitDir} ${addOnName})
+
+    set_source_files_properties (${AddOnSourceFiles} PROPERTIES LANGUAGE CXX)
+    SetCompilerOptions (${addOnName} ${acVersion})
+
+endfunction ()
