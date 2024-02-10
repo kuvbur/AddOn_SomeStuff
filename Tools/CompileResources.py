5d3a9be3ff27125953b515964f39c4f6b8bc6d5b
Дмитрий <kuvbur@gmail.com>
2024-02-10 17:32:32 +0300

    Добавил замену на светлые иконки

--- Tools/CompileResources.py
+++ Tools/CompileResources.py
@@ -183,6 +183,13 @@ class MacResourceCompiler (ResourceCompiler):
                 stringsFile = codecs.open(filePath, 'r', 'utf-16')
                 resultLocalizableStringsFile.write(stringsFile.read())
                 stringsFile.close()
+        icondark = os.path.join(os.path.dirname(
+            self.resourceObjectsPath), 'MacDarkModeIcon')
+        for fileName in os.listdir(icondark):
+            filePath = os.path.join(icondark, fileName)
+            extension = os.path.splitext(fileName)[1].lower()
+            if extension == '.tif':
+                shutil.copy(filePath, resultResourcePath)
         resultLocalizableStringsFile.close()
 
 
