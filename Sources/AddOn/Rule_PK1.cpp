e9a07e351e43c95cb184cb8ebb2f761094e67398
Дмитрий <kuvbur@gmail.com>
2024-03-02 23:17:25 +0300

    Добавил заготовку для правил

---
+++ Sources/AddOn/Rule_PK1.cpp
@@ -0,0 +1,16 @@
+﻿//------------ kuvbur 2022 ------------
+#include	"APIEnvir.h"
+#include	"ACAPinc.h"
+#include	"Rule_PK1.hpp"
+
+void ReservationCHandler(const GS::HashTable<API_Guid, short>& reserved,
+	const GS::HashSet<API_Guid>& released,
+	const GS::HashSet<API_Guid>& deleted) {
+	int hh = 1;
+	return;
+}
+
+void ElementEventHandler(const API_NotifyElementType* elemType, API_ActTranPars& actTranPars) {
+	int hh = 1;
+	return;
+}
