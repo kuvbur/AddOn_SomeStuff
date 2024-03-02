c99187b50e698daa20dbbfddfa80c4d380240981
Дмитрий <kuvbur@gmail.com>
2024-03-02 19:32:04 +0300

    Update Revision.cpp

--- Sources/AddOn/Revision.cpp
+++ Sources/AddOn/Revision.cpp
@@ -5,45 +5,54 @@
 #include	"ACAPinc.h"
 #include	"Revision.hpp"
 
-// -----------------------------------------------------------------------------
-// Get the Changes from the current Document Revision of the given Layout
-// -----------------------------------------------------------------------------
+typedef struct
+{
+	GS::Array<API_Guid> guids;
+} Revision;
+
+typedef std::map<std::string, Revision, doj::alphanum_less<std::string>> RevisionDict;
 
 void		Do_GetLayoutCurrentRevisionChanges(void)
 {
+	GSErrCode err = NoError;
 	API_DatabaseInfo dbInfo;
 	BNZeroMemory(&dbInfo, sizeof(API_DatabaseInfo));
-	GSErrCode err = ACAPI_Database(APIDb_GetCurrentDatabaseID, &dbInfo);
-	if (err != NoError) {
-		ACAPI_WriteReport("Do_GetLayoutCurrentRevisionChanges: error occured!", false);
-		return;
+	err = ACAPI_Database(APIDb_GetCurrentDatabaseID, &dbInfo);
+	if (err != NoError) return;
+	if (dbInfo.typeID != APIWind_LayoutID) return;
+	API_ElemTypeID elementType = API_ChangeMarkerID;
+	GS::Array<API_Guid>	guidArray;
+	err = ACAPI_Element_GetElemList(elementType, &guidArray, APIFilt_IsEditable | APIFilt_HasAccessRight | APIFilt_InMyWorkspace | APIFilt_OnVisLayer);
+	if (guidArray.IsEmpty()) return;
+	API_Element element = {};
+	for (UInt32 i = 0; i < guidArray.GetSize(); i++) {
+		element.header.guid = guidArray[i];
+		err = ACAPI_Element_Get(&element);
+
+		if (err != NoError) {
+		}
 	}
 
-	if (dbInfo.typeID != APIWind_LayoutID) {
-		ACAPI_WriteReport("Current database not a layout database!", false);
-		return;
-	}
+	//GS::Array<API_RVMChange> changes;
+	//err = ACAPI_Database(APIDb_GetRVMLayoutCurrentRevisionChangesID, &(dbInfo.databaseUnId), &changes);
+	//if (err != NoError) {
+	//	ACAPI_WriteReport("Do_GetLayoutCurrentRevisionChanges: error occured!", false);
+	//	return;
+	//}
 
-	GS::Array<API_RVMChange> changes;
-	err = ACAPI_Database(APIDb_GetRVMLayoutCurrentRevisionChangesID, &(dbInfo.databaseUnId), &changes);
-	if (err != NoError) {
-		ACAPI_WriteReport("Do_GetLayoutCurrentRevisionChanges: error occured!", false);
-		return;
-	}
+	//char buffer[256];
 
-	char buffer[256];
+	//if (changes.IsEmpty()) {
+	//	sprintf(buffer, "There are no changes in current revision of {%s} layout!", APIGuid2GSGuid(dbInfo.databaseUnId.elemSetId).ToUniString().ToCStr().Get());
+	//	ACAPI_WriteReport(buffer, false);
+	//	return;
+	//}
 
-	if (changes.IsEmpty()) {
-		sprintf(buffer, "There are no changes in current revision of {%s} layout!", APIGuid2GSGuid(dbInfo.databaseUnId.elemSetId).ToUniString().ToCStr().Get());
-		ACAPI_WriteReport(buffer, false);
-		return;
-	}
+	//sprintf(buffer, "# Changes in current revision of {%s} layout:", APIGuid2GSGuid(dbInfo.databaseUnId.elemSetId).ToUniString().ToCStr().Get());
+	//ACAPI_WriteReport(buffer, false);
 
-	sprintf(buffer, "# Changes in current revision of {%s} layout:", APIGuid2GSGuid(dbInfo.databaseUnId.elemSetId).ToUniString().ToCStr().Get());
-	ACAPI_WriteReport(buffer, false);
-
-	for (auto& change : changes.AsConst()) {
-		sprintf(buffer, "ID: %s, Description: %s", change.id.ToCStr().Get(), change.description.ToCStr().Get());
-		ACAPI_WriteReport(buffer, false);
-	}
+	//for (auto& change : changes.AsConst()) {
+	//	sprintf(buffer, "ID: %s, Description: %s", change.id.ToCStr().Get(), change.description.ToCStr().Get());
+	//	ACAPI_WriteReport(buffer, false);
+	//}
 }
