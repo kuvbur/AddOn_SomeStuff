3a1f842822c6c29c282db3c4169bb710e51ad067
Дмитрий <kuvbur@gmail.com>
2024-05-28 01:01:29 +0300

    Запись геометрии фасонки из выделенных линий ч 1

--- Sources/AddOn/AutomateFunction.cpp
+++ Sources/AddOn/AutomateFunction.cpp
@@ -27,75 +27,93 @@ namespace AutoFunc {
 			element.header.guid = lines[i];
 			if(ACAPI_Element_Get(&element) == NoError) {
 				coords.Push(element.line.begC);
-				coords.Push(element.line.endC);
 			}
 		}
-		ParamDictValue params;
+		ACAPI_CallUndoableCommand("undoString", [&] () -> GSErrCode {
+			GSErrCode err = WriteGDLValues(elements.Get(0), coords);
 
+			return err;
+			});
 	}
 
-	bool WriteGDLValues(API_Guid elemGuid, ParamDictValue& params) {
-		if(params.IsEmpty()) return false;
-		if(elemGuid == APINULLGuid) return false;
+	GSErrCode WriteGDLValues(API_Guid elemGuid, GS::Array<API_Coord>& coords) {
+		if(coords.IsEmpty()) return APIERR_GENERAL;
+		if(elemGuid == APINULLGuid) return APIERR_GENERAL;
+		GSErrCode err = NoError;
 		API_Elem_Head elem_head = {};
+		API_Element element = {};
+		API_Element mask = {};
 		elem_head.guid = elemGuid;
-		if(ACAPI_Element_GetHeader(&elem_head) != NoError) return false;
-
+		err = ACAPI_Element_GetHeader(&elem_head); if(err != NoError) return err;
+		element.header.guid = elemGuid;
+		err = ACAPI_Element_Get(&element); if(err != NoError) return err;
 		API_ParamOwnerType	apiOwner = {};
 		API_GetParamsType	apiParams = {};
+		BNZeroMemory(&apiOwner, sizeof(API_ParamOwnerType));
+		BNZeroMemory(&apiParams, sizeof(API_GetParamsType));
 		apiOwner.guid = elemGuid;
 		apiOwner.typeID = elem_head.typeID;
-		if(ACAPI_Goodies(APIAny_OpenParametersID, &apiOwner, nullptr) != NoError) return false;
-		if(ACAPI_Goodies(APIAny_GetActParametersID, &apiParams) != NoError) {
-			ACAPI_Goodies(APIAny_CloseParametersID);
-			return false;
+		err = ACAPI_Goodies(APIAny_OpenParametersID, &apiOwner, nullptr);
+		if(err != NoError) {
+			ACAPI_Goodies(APIAny_CloseParametersID, nullptr, nullptr);
+			return err;
 		}
-
+		err = ACAPI_Goodies(APIAny_GetActParametersID, &apiParams);
+		if(err != NoError) {
+			ACAPI_Goodies(APIAny_CloseParametersID, nullptr, nullptr);
+			return err;
+		}
+		Int32 n_t = coords.GetSize();
+		Int32 inx_kontur = 0;
 		API_ChangeParamType	chgParam;
-
-		bool flagFind = false;
-		Int32	addParNum = BMGetHandleSize((GSHandle) apiParams.params) / sizeof(API_AddParType);
-		Int32 nfind = params.GetSize();
+		Int32 addParNum = BMGetHandleSize((GSHandle) apiParams.params) / sizeof(API_AddParType);
 		for(Int32 i = 0; i < addParNum; ++i) {
-			API_AddParType& actualParam = (*apiParams.params)[i];
-			GS::UniString name = actualParam.name;
-			GS::UniString rawname = "{@gdl:" + name.ToLowerCase() + "}";
-			if(params.ContainsKey(rawname)) {
-				ParamValueData paramfrom = params.Get(rawname).val;
+			GS::UniString name = (*apiParams.params)[i].name;
+			if(name.IsEqual("kontur") && (*apiParams.params)[i].typeMod == API_ParArray) {
+				inx_kontur = i;
 				BNZeroMemory(&chgParam, sizeof(API_ChangeParamType));
-				chgParam.index = actualParam.index;
-				CHTruncate(actualParam.name, chgParam.name, API_NameLen);
-				if(actualParam.typeID == APIParT_CString) {
-					GS::uchar_t uStrBuffer[256];
-					GS::ucsncpy(uStrBuffer, paramfrom.uniStringValue.ToUStr().Get(), 256);
-					chgParam.uStrValue = uStrBuffer;
-				}
-				if(actualParam.typeID == APIParT_Integer) {
-					chgParam.realValue = paramfrom.intValue;
-				}
-				if(actualParam.typeID == APIParT_Length) {
-					chgParam.realValue = paramfrom.doubleValue;
-				}
-				if(actualParam.typeID == APIParT_Angle) {
-					chgParam.realValue = paramfrom.doubleValue;
-				}
-				if(actualParam.typeID == APIParT_RealNum) {
-					chgParam.realValue = paramfrom.doubleValue;
-				}
-				if(actualParam.typeID == APIParT_Boolean) {
-					chgParam.realValue = paramfrom.boolValue;
-				}
-				if(ACAPI_Goodies(APIAny_ChangeAParameterID, &chgParam, nullptr) != NoError) return false;
+				chgParam.index = (*apiParams.params)[i].index;
+				CHTruncate((*apiParams.params)[i].name, chgParam.name, API_NameLen);
+				chgParam.realValue = 0;
+				chgParam.ind1 = (*apiParams.params)[i].dim1;
+				chgParam.ind2 = (*apiParams.params)[i].dim2;
+				err = ACAPI_Goodies(APIAny_ChangeAParameterID, &chgParam, nullptr); if(err != NoError) return err;
+			}
+			if(name.IsEqual("n_t")) {
+				BNZeroMemory(&chgParam, sizeof(API_ChangeParamType));
+				chgParam.index = (*apiParams.params)[i].index;
+				CHTruncate((*apiParams.params)[i].name, chgParam.name, API_NameLen);
+				chgParam.realValue = n_t;
+				err = ACAPI_Goodies(APIAny_ChangeAParameterID, &chgParam, nullptr); if(err != NoError) return err;
 			}
 		}
+		err = ACAPI_Goodies(APIAny_GetActParametersID, &apiParams); if(err != NoError) return err;
+		err = ACAPI_Goodies(APIAny_CloseParametersID, nullptr, nullptr); if(err != NoError) return err;
 
-		if(ACAPI_Goodies(APIAny_GetActParametersID, &apiParams) != NoError) return false;
-		if(ACAPI_Goodies(APIAny_CloseParametersID) != NoError) return false;
-
+		Int32 inDim1 = (*apiParams.params)[inx_kontur].dim1;
+		Int32 inDim2 = (*apiParams.params)[inx_kontur].dim2;
+		size_t ind = 0;
+		double** newArrHdl = (double**)(*apiParams.params)[inx_kontur].value.array;
+		for(Int32 i = 0; i < inDim1; i++) {
+			if (i < n_t){
+				(*newArrHdl)[ind] = coords.Get(i).x;
+				ind++;
+				(*newArrHdl)[ind] = coords.Get(i).y;
+				ind++;
+			} else{
+				(*newArrHdl)[ind] = 0;
+				ind++;
+				(*newArrHdl)[ind] = 0;
+				ind++;
+			}
+		}
 		API_ElementMemo	elemMemo = {};
+		BNZeroMemory(&elemMemo, sizeof(elemMemo));
+		(*apiParams.params)[inx_kontur].value.array = (GSHandle) newArrHdl;
 		elemMemo.params = apiParams.params;
-		GSErrCode err = ACAPI_Element_ChangeMemo(elemGuid, APIMemoMask_AddPars, &elemMemo);
+		ACAPI_ELEMENT_MASK_CLEAR(mask);
+		err = ACAPI_Element_Change(&element, &mask, &elemMemo, APIMemoMask_AddPars, true);
 		ACAPI_DisposeAddParHdl(&apiParams.params);
-		return (err == NoError);
+		return err;
 	}
 }
