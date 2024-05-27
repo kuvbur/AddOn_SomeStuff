acb8552f5a0c91bd9e851ccf6c15774baaa06658
Dmitry Rogozhin <kuvbur@gmail.com>
2024-05-27 20:59:42 +0300

    up

--- Sources/AddOn/AutomateFunction.cpp
+++ Sources/AddOn/AutomateFunction.cpp
@@ -16,7 +16,7 @@ namespace AutoFunc {
 			if(ACAPI_Element_GetHeader(&elem_head) == NoError) {
 				API_ElemTypeID elementType = elem_head.typeID;
 				if (elementType == API_ObjectID) elements.Push(guidArray[i]);
-				if (elementType == API_PolyLineID || API_LineID) lines.Push(guidArray[i]);
+				if (elementType == API_PolyLineID || elementType == API_LineID) lines.Push(guidArray[i]);
 			}
 		}
 		if (elements.IsEmpty() || lines.IsEmpty()) return;
@@ -30,6 +30,72 @@ namespace AutoFunc {
 				coords.Push(element.line.endC);
 			}
 		}
-		int hh=1;
+		ParamDictValue params;
+
+	}
+
+	bool WriteGDLValues(API_Guid elemGuid, ParamDictValue& params) {
+		if(params.IsEmpty()) return false;
+		if(elemGuid == APINULLGuid) return false;
+		API_Elem_Head elem_head = {};
+		elem_head.guid = elemGuid;
+		if(ACAPI_Element_GetHeader(&elem_head) != NoError) return false;
+
+		API_ParamOwnerType	apiOwner = {};
+		API_GetParamsType	apiParams = {};
+		apiOwner.guid = elemGuid;
+		apiOwner.typeID = elem_head.typeID;
+		if(ACAPI_Goodies(APIAny_OpenParametersID, &apiOwner, nullptr) != NoError) return false;
+		if(ACAPI_Goodies(APIAny_GetActParametersID, &apiParams) != NoError) {
+			ACAPI_Goodies(APIAny_CloseParametersID);
+			return false;
+		}
+
+		API_ChangeParamType	chgParam;
+
+		bool flagFind = false;
+		Int32	addParNum = BMGetHandleSize((GSHandle) apiParams.params) / sizeof(API_AddParType);
+		Int32 nfind = params.GetSize();
+		for(Int32 i = 0; i < addParNum; ++i) {
+			API_AddParType& actualParam = (*apiParams.params)[i];
+			GS::UniString name = actualParam.name;
+			GS::UniString rawname = "{@gdl:" + name.ToLowerCase() + "}";
+			if(params.ContainsKey(rawname)) {
+				ParamValueData paramfrom = params.Get(rawname).val;
+				BNZeroMemory(&chgParam, sizeof(API_ChangeParamType));
+				chgParam.index = actualParam.index;
+				CHTruncate(actualParam.name, chgParam.name, API_NameLen);
+				if(actualParam.typeID == APIParT_CString) {
+					GS::uchar_t uStrBuffer[256];
+					GS::ucsncpy(uStrBuffer, paramfrom.uniStringValue.ToUStr().Get(), 256);
+					chgParam.uStrValue = uStrBuffer;
+				}
+				if(actualParam.typeID == APIParT_Integer) {
+					chgParam.realValue = paramfrom.intValue;
+				}
+				if(actualParam.typeID == APIParT_Length) {
+					chgParam.realValue = paramfrom.doubleValue;
+				}
+				if(actualParam.typeID == APIParT_Angle) {
+					chgParam.realValue = paramfrom.doubleValue;
+				}
+				if(actualParam.typeID == APIParT_RealNum) {
+					chgParam.realValue = paramfrom.doubleValue;
+				}
+				if(actualParam.typeID == APIParT_Boolean) {
+					chgParam.realValue = paramfrom.boolValue;
+				}
+				if(ACAPI_Goodies(APIAny_ChangeAParameterID, &chgParam, nullptr) != NoError) return false;
+			}
+		}
+
+		if(ACAPI_Goodies(APIAny_GetActParametersID, &apiParams) != NoError) return false;
+		if(ACAPI_Goodies(APIAny_CloseParametersID) != NoError) return false;
+
+		API_ElementMemo	elemMemo = {};
+		elemMemo.params = apiParams.params;
+		GSErrCode err = ACAPI_Element_ChangeMemo(elemGuid, APIMemoMask_AddPars, &elemMemo);
+		ACAPI_DisposeAddParHdl(&apiParams.params);
+		return (err == NoError);
 	}
 }
