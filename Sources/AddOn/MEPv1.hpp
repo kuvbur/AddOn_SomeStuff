5dedd9c1bdd60bdb192ea1797685615fe8fca0d2
Dmitry Rogozhin <kuvbur@gmail.com>
2024-07-10 12:38:02 +0300

    Поправил поломанную обратную совместимость

--- Sources/AddOn/MEPv1.hpp
+++ Sources/AddOn/MEPv1.hpp
@@ -34,31 +34,12 @@
 #include "ACAPI/MEPEquipmentDefault.hpp"
 #include "ACAPI/MEPUniqueID.hpp"
 #include "ACAPI/MEPPreferenceTableContainerBase.hpp"
+#include "ACAPI/MEPDuctPreferenceTableContainer.hpp"
+#include "ACAPI/MEPPipePreferenceTableContainer.hpp"
+#include "ACAPI/MEPCableCarrierPreferenceTableContainer.hpp"
+#include "ACAPI/MEPPreferenceTableBase.hpp"
 #include "GSUnID.hpp"
 
-#include "ACAPI/MEPAdapter.hpp"
-
-#include "ACAPI/MEPElement.hpp"
-#include "ACAPI/MEPModifiableElement.hpp"
-
-#include "ACAPI/MEPPort.hpp"
-#include "ACAPI/MEPVentilationPort.hpp"
-#include "ACAPI/MEPPipingPort.hpp"
-
-#include "ACAPI/MEPRoutingElement.hpp"
-#include "ACAPI/MEPRoutingElementDefault.hpp"
-#include "ACAPI/MEPRoutingSegment.hpp"
-#include "ACAPI/MEPRoutingSegmentDefault.hpp"
-#include "ACAPI/MEPRigidSegmentDefault.hpp"
-#include "ACAPI/MEPRoutingNodeDefault.hpp"
-#include "ACAPI/MEPBendDefault.hpp"
-#include "ACAPI/MEPTakeOff.hpp"
-#include "ACAPI/MEPTransitionDefault.hpp"
-
-#include "ACAPI/MEPBranch.hpp"
-#include "ACAPI/MEPTerminal.hpp"
-#include "ACAPI/MEPFitting.hpp"
-#include "ACAPI/MEPFlexibleSegment.hpp"
 
 namespace MEPv1 {
     void GetSubElementOfRouting (const API_Guid& elemGuid, GS::Array<API_Guid>& subelemGuid);
