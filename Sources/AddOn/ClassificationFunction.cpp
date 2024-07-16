c2aec488e5ff5838e7eba56af7c9ac281b4d8046
Dmitry Rogozhin <kuvbur@gmail.com>
2024-07-16 15:16:58 +0300

    Добавлен файл для классификации

---
+++ Sources/AddOn/ClassificationFunction.cpp
@@ -0,0 +1,9 @@
+﻿//------------ kuvbur 2022 ------------
+#include	"ACAPinc.h"
+#include	"APIEnvir.h"
+#include	"ClassificationFunction.hpp"
+#include	"Helpers.hpp"
+
+namespace ClassificationFunc
+{
+}
---
+++ Sources/AddOn/ClassificationFunction.hpp
@@ -0,0 +1,22 @@
+//------------ kuvbur 2022 ------------
+#if !defined (CLASSIF_HPP)
+#define	CLASSIF_HPP
+#ifdef AC_25
+#include "APICommon25.h"
+#endif // AC_25
+#ifdef AC_26
+#include "APICommon26.h"
+#endif // AC_26
+#ifdef AC_27
+#include "APICommon27.h"
+#endif // AC_27
+#ifdef AC_28
+#include "APICommon28.h"
+#endif // AC_28
+
+namespace ClassificationFunc
+{
+
+}
+
+#endif
