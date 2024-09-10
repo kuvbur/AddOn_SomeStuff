3c62c515b0f07a46e87f5f56e44a4fb7db7650b4
Dmitry Rogozhin <kuvbur@gmail.com>
2024-09-10 18:10:37 +0300

    Правка подсчёта изменений на макетах

--- Sources/AddOn/Revision.hpp
+++ Sources/AddOn/Revision.hpp
@@ -26,31 +26,33 @@ typedef struct
     API_Guid markerguid = APINULLGuid;
     GS::UniString changeId = "";
     GS::UniString changeName = "";
-    GS::UniString text = ""; // Текст изменения
-    GS::UniString number = "";
-    GS::UniString inx = "";
+    GS::UniString note = ""; // Текст изменения
+    GS::UniString nizm = ""; // Номер изменения
+    GS::UniString nuch = ""; // Номер участка
     GS::Int32 typeizm = TypeNone;
-} Change;
+} Change; // Хранение одного изменения (облака)
 
 typedef struct
 {
     GS::Array<Change> arr;
-    GS::Int32 nuch = 0;
-} Changes;
+    GS::Int32 nuch = 0; // Количество участков
+} Changes; // Массив изменений на листе
 
-typedef std::map<std::string, Changes, doj::alphanum_less<std::string>> ChangeMarkerDict;
+typedef std::map<std::string, Changes, doj::alphanum_less<std::string>> ChangeMarkerDict; // Словарь изменений листа, ключ - Имя изменения (П АР Изм 0)
 
 typedef std::map<std::string, ChangeMarkerDict, doj::alphanum_less<std::string>> ChangeMarkerByListDict;
 
 static void Do_GetChangeCustomScheme (void);
 
-void ChangeMarkerText (API_Guid& markerguid, GS::UniString& number, GS::UniString& inx);
+void ChangeMarkerText (API_Guid& markerguid, GS::UniString& nuch, GS::UniString& nizm);
 
 bool GetMarkerPos (API_Guid& markerguid, API_Coord& startpoint);
 
-bool GetMarkerText (API_Guid& markerguid, GS::UniString& text, GS::UniString& number, GS::UniString& inx, GS::Int32& type);
+bool GetMarkerText (API_Guid& markerguid, GS::UniString& note, GS::UniString& nuch, GS::UniString& nizm, GS::Int32& typeizm);
 
 bool GetChangesMarker (ChangeMarkerDict& changes);
+
+void GetScheme ();
 
 bool GetAllChangesMarker (void);
 
